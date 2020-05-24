5dc1d3a22d623627946ad6a8dc15fd3039bd8b27
Héctor Galván <inghgalvan@outlook.com>
2020-05-24 01:17:04 -0500

    portafolio 75%

--- portafolio/portafolio/views.py
+++ portafolio/portafolio/views.py
@@ -4,20 +4,20 @@ from django.shortcuts import render, HttpResponse
 
 #-------------portada------------
 def home(request):
-    return render(request, "core/home.html")
+    return render(request, "users/home.html")
 
 #-------------acerca de------------
 def about(request):
-    return render(request, "core/about.html")
+    return render(request, "users/about.html")
 
 #-------------Portafolio------------
 def portafolio(request):
-    return render(request, "core/portafolio.html")
+    return render(request, "users/portafolio.html")
 
 #-------------Contacto--------------------
 def contact(request):
-    return render(request, "core/contacto.html")
+    return render(request, "users/contacto.html")
 
 #-------------Certificaciones--------------
 def certificaciones(request):
-    return render(request, "core/certificaciones.html")
+    return render(request, "users/certificaciones.html")
